ecd2b11bdbf986371b9afe72151021dc4cd2fa2b
21900162 <s21900162@walab.handong.edu>
2022-04-15 17:55:16 +0900

    가격검색  함수생성

--- manager.c
+++ manager.c
@@ -89,3 +89,21 @@ void searchName(Product * m, int index){
  printf("\n");
 }
 
+void searchPrice(Product * m, int index){
+ int price=0;
+ int check=0;
+ printf("검색할 가격? ");
+ scanf("%d", &price);
+ for(int i=0; i<index; i++){
+ if(m[i].price == -1) continue;
+ if(m[i].price == price){
+ printf("\n%d번\n", i+1);
+ readPr(m[i]);
+ check++;
+ }
+ }
+ if(check==0) printf("검색된 데이터 없음");
+ printf("\n");
+}
+
+
--- manager.h
+++ manager.h
@@ -8,4 +8,4 @@ int selectPr(Product * m, int index);//제품 고르기 함수
 int loadfile(Product * m);//파일 불러오기 함수
 void savefile(Product * m, int index);//파일 저장함수
 void searchName(Product * m, int index);//제품명 검색함수
-
+void searchPrice(Product * m, int index);//가격 검색함수
